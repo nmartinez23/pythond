0d38e66ce55663cf6eb6d5a60cf948bc42150907
Nick Martinez <23nicholasmartinez@gmail.com>
2016-05-17 21:17:46 -0700

    Parent child inheritance in python

---
+++ inheritance.py
@@ -0,0 +1,20 @@
+class Parent():
+    def __init__(self, last_name, eye_color):
+        print("Parent Constructor Called")
+        self.last_name = last_name
+        self.eye_color = eye_color
+
+    def show_info(self):
+        print("Last Name - "+self.last_name)
+        print("Eye Color - "+self.eye_color)
+
+class Child(Parent):
+    def __init__(self, last_name, eye_color, number_of_toys):
+        print("Child Constructor Called")
+        Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = number_of_toys
+
+nick_m = Parent("Martinez", "brown")
+nick_m.show_info()
+kayla_m = Child("Martinez", "brown", 8)
+kayla_m.show_info() #instance module inherited from Parent class
