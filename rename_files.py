69b8c3f73110b3dee2da53050448b3b7ffc52712
Nick Martinez <23nicholasmartinez@gmail.com>
2016-05-20 14:38:17 -0700

    python code to rename files

---
+++ rename_files.py
@@ -0,0 +1,18 @@
+import os
+
+def rename_files():
+    #1 get file names from a folder
+    file_list = os.listdir(r"C:\OOP\prank")
+    saved_path = os.getcwd()
+    print("Current Working Directory is "+saved_path)
+    os.chdir(r"C:\OOP\prank")
+
+    #2 for each file, rename filename...takes out any numbers in filename
+    for file_name in file_list:
+        print("Old Name = "+file_name)
+        print("New Name = "+file_name.translate(None, "0123456789"))
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+    os.chdir(saved_path)
+
+rename_files()
+    
