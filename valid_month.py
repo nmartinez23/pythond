7c4f4c266bd35a7aefbea4cf3ffcc503e963a078
Nick Martinez <23nicholasmartinez@gmail.com>
2017-03-15 20:56:04 -0700

    Create valid_month.py

---
+++ valid_month.py
@@ -0,0 +1,62 @@
+# -----------
+# User Instructions
+# 
+# Modify the valid_month() function to verify 
+# whether the data a user enters is a valid 
+# month. If the passed in parameter 'month' 
+# is not a valid month, return None. 
+# If 'month' is a valid month, then return 
+# the name of the month with the first letter 
+# capitalized.
+#
+
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+          
+# def valid_month(month):
+#     month = month.title()
+#     for i in months:
+#         if i == month:
+#             return month
+    
+#     return None
+
+# refactored
+# def valid_month(month):
+#     if month:
+#         cap_month = month.capitalize()
+#         if cap_month in months:
+#             return cap_month
+
+# refactored with dictionary to map first three letters of each month
+# user only has to input first three letters correctly
+month_abbvs = dict((m[:3].lower(), m) for m in months)
+
+def valid_month(month):
+    if month:
+        short_month = month[:3].lower()
+        return month_abbvs.get(short_month)
+
+
+print valid_month("january") 
+# => "January"    
+print valid_month("Janruary") 
+# => "January"
+print valid_month("feb")
+# => February
+print valid_month("octuber")
+# => October
+print valid_month("foo")
+# => None
+print valid_month("")
+# => None
