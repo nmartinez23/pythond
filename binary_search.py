cdb55ac1fd397d2cd0d8ff23b2009d3566fde888
Nick Martinez <23nicholasmartinez@gmail.com>
2017-10-29 15:26:56 -0700

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,17 +1,18 @@
 def binary_search(input_array, value):
     low = 0
     high = len(input_array) - 1
+    input_array.sort()
     while (low <= high):
 	    mid = (low + high) / 2
 	    if (input_array[mid] == value):
-		    return mid
+		    return value
 	    elif (input_array[mid] < value):
 		    low = mid + 1
 	    else:
 		    high = mid - 1
     return -1
 
-test_list = [1,3,9,11,15,19,29]
+test_list = [1,9,3,11,15,19,29]
 test_val1 = 25
 test_val2 = 15
 
@@ -19,4 +20,4 @@ print binary_search(test_list, test_val1)
 # 25 not found, returns - 1
 
 print binary_search(test_list, test_val2)
-# returns index position 4
+# returns 15
